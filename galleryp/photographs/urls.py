87ef3f5bfebd92ef8f455398c3241c8e9646fde9
maureen-james <manjax97@gmail.com>
2022-05-27 15:08:41 +0300

    feat:add photos template

--- galleryp/photographs/urls.py
+++ galleryp/photographs/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns=[
     path('',views.welcome,name = 'welcome'),
-    path('archives/(\d{4}-\d{2}-\d{2})/',views.past_days_news,name = 'pastNews'), 
+    # path('archives/(\d{4}-\d{2}-\d{2})/',views.past_days_news,name = 'pastNews'), 
 ]
--- galleryp/photographs/views.py
+++ galleryp/photographs/views.py
@@ -8,13 +8,13 @@ from django.core.exceptions import ObjectDoesNotExist
 # Create your views here.
 
 
-def news_today(request):
+def todays_photos(request):
     date = dt.date.today()
-    return render(request, 'all-news/today-news.html',{'date': date})
+    return render(request, 'photos/todays_photos.html',{'date': date})
 
 
 
-def past_days_news(request, past_date):
+def past_days_photographs(request, past_date):
     try:
         # Converts data from the string Url
         date = dt.datetime.strptime(past_date, '%Y-%m-%d').date()
@@ -24,15 +24,15 @@ def past_days_news(request, past_date):
         assert False
 
     if date == dt.date.today():
-        return redirect(news_today)
+        return redirect(todays_photos)
 
     news = Article.days_news(date)
-    return render(request, 'all-news/past-news.html',{"date": date,"news":news})
+    return render(request, 'photos/pastphotos.html',{"date": date,"news":news})
 
-def news_today(request):
+def todays_photos(request):
     date = dt.date.today()
     news = Article.todays_news()
-    return render(request, 'all-news/today-news.html', {"date": date,"news":news})  
+    return render(request, 'photos/todays_photos.html', {"date": date,"news":news})  
 
 def search_results(request):
 
@@ -41,15 +41,15 @@ def search_results(request):
         searched_articles = Article.search_by_title(search_term)
         message = f"{search_term}"
 
-        return render(request, 'all-news/search.html',{"message":message,"articles": searched_articles})
+        return render(request, 'photos/search.html',{"message":message,"articles": searched_articles})
 
     else:
         message = "You haven't searched for any term"
-        return render(request, 'all-news/search.html',{"message":message})  
+        return render(request, 'photos/search.html',{"message":message})  
 
 def article(request,article_id):
     try:
         article = Article.objects.get(id = article_id)
     except ObjectDoesNotExist:
         raise Http404()
-    return render(request,"all-news/article.html", {"article":article})            
+    return render(request,"photos/article.html", {"article":article})            
