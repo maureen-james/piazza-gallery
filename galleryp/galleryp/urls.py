100053575c48a145b244bc9ed4d3e72cdefdf0a3
maureen-james <manjax97@gmail.com>
2022-05-29 19:14:34 +0300

    Modify images templates

--- galleryp/galleryp/urls.py
+++ galleryp/galleryp/urls.py
@@ -19,6 +19,6 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('photographs/',include('photographs.urls'))
+    path('',include('photographs.urls'))
 
 ]
--- galleryp/photographs/admin.py
+++ galleryp/photographs/admin.py
@@ -5,6 +5,9 @@ from django.contrib import admin
 from .models import Image,Category,Location
 # Register your models here.
 
+# class ImageAdmin(admin.ModelAdmin):
+#     filter_horizontal =('category',)
+
 admin.site.register(Image)
 admin.site.register(Category)
 admin.site.register(Location)
