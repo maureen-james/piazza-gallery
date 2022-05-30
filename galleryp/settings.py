a270c0d6372d471f64e0c91f7c618d2ec71993ba
maureen-james <manjax97@gmail.com>
2022-05-31 02:00:57 +0300

    add CSRF debug

--- galleryp/settings.py
+++ galleryp/settings.py
@@ -53,6 +53,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+CSRF_COOKIE_SECURE = False
 
 ROOT_URLCONF = 'galleryp.urls'
 
